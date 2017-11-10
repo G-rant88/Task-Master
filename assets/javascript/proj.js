a666f2f79dfaba07d803da02ef0539272d83b9cf
unknown <benjah88@gmail.com>
2017-11-10 14:40:04 -0800

    lat and long

--- assets/javascript/proj.js
+++ assets/javascript/proj.js
@@ -1,7 +1,8 @@
 // var query = $("#thing1").val();
 // var location = $("#starts").val();
 // var url = "https://api.foursquare.com/v2/venues/search?&query=" + query + "&mode=url&limit=5&near=" + location + "&client_id=YDAI3HM532ZHCLP4XBC4Z5OCOO2YN5JT3Q3SP4C3EKDNZKT5&client_secret=UQKRY5R4YPYKDQAQNUCVSSVGGPMUXPQA1XSIJOIQT5ZDO4HU&v=20140806&m=foursquare";
-
+var lat;
+var lng;
 	var p = $("<p>");
 $("#sub1").on("click", function(){
 
@@ -26,28 +27,64 @@ method: "GET"
 
 for (var i = 0; i < 5; i++) {
 		console.log(call);
+	var newDiv = $("<div>");
+	 newDiv.addClass("things");
 	 p = $("<p>");
+	 p2 = $("<p>");
+	 p3 = $("<p>");
 	// var p2 = $("<p>");
 	// var p3 = $("<p>");
-	var br = $("<br>")
-	var br2 = $("<br>")
+	// var br = $("<br>")
+	// var br2 = $("<br>")
 	p.append("Name: " + call.response.venues[i].name);
-	$("#list").append(p);
-	p.append(br2);
+	p2.append("Location: " + call.response.venues[i].location.formattedAddress);
+	p3.append("Category: " + call.response.venues[i].categories[0].name);
+	 
+	newDiv.append(p, p2, p3);
+ lat = call.response.venues[i].location.lat;
+	 lng = call.response.venues[i].location.lng;
+
+	 // lat.push(lats);
+	 // lng.push(lngs);
+
+
+
+	$("#list").append(newDiv);
+
 	
-p.append("Location: " + call.response.venues[i].location.formattedAddress);
-$("#list").append(p);
-p.append(br);
-p.append("Category: " + call.response.venues[i].categories[0].name);
- $("#list").append(p);
+// p.append("Location: " + call.response.venues[i].location.formattedAddress);
+// $("#list").append(p);
+
+// p.append("Category: " + call.response.venues[i].categories[0].name);
+//  $("#list").append(p);
 }
-});
+
+
+ 
 
 });
 
-// $(document).on("click", p, function() {
+});
+
+ $(document).on("click", ".things", function() {
+var pp = $("<p>");
+pp.append("Your Choice: ")
+$("#thinghere").append(pp);
+ $("#thinghere").append(this);
 
 
-// $("#thinghere").append(this);
+ $("#list").html("");
+ $("#thing1").val("");
+ console.log(lat);
+ console.log(lng);
 
-// });
+
+ });
+ // $(document).on("click", "#route", function() {
+
+
+ // 	console.log();
+ // 	console.log();
+ // 	console.log();
+
+ // });
