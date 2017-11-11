70ec672d74550b39fb184ebe4e467c032a35bcfd
Min Ryu <myyms@Mins-MacBook-Pro.local>
2017-11-10 19:25:25 -0800

    displaying route with one to do complete

--- assets/javascript/proj.js
+++ assets/javascript/proj.js
@@ -1,180 +1,170 @@
-// var key = "AIzaSyBm04RwZjjDTqmjQlDjzAH3zSAJwSSlIgs";
-// var start = "10995+Le+Conte+Ave&destination=10995+Le+Conte+Ave";
-// var end = "10995+Le+Conte+Ave&destination=10995+Le+Conte+Ave";
-// var search = "restaurants";
-// var waypoints = "10995+Le+Conte+Ave&destination=10995+Le+Conte+Ave"
-// var mapsUrl = "https://maps.googleapis.com/maps/api/js?key=" + key + "&callback=initMap";
-// var placesUrl = "https://maps.googleapis.com/maps/api/js?key=" + key + "&libraries=places";
-// var home = "123 Main St, Northeast Harbor, ME 04662, United States";
+$(document).ready(function() {
 
 
-
-// var map;
-// var service;
-// var infowindow;
-// var search = $("#thing1").val();
+	var lat = [];
+	var lng = [];
+	var p = $("<p>");
 
 
+	//
+	$("#sub1").on("click", function(){
 
+		var location = $("#starts").val().trim();
+		$("#starthere").html("Searching Near: " + location);
 
-// // service = new google.maps.places.PlacesService(map);
-// // service.textSearch(request, callback);
+	});
 
+	//
+	$("#sub3").on("click", function(){
+	
+		lat = [];
+		lng = [];
 
-// function initialize() {
-//   var home = new google.maps.LatLng(44.29371940000001, -68.2890729);
+		var query = $("#thing1").val().trim();
+		var location = $("#starts").val().trim();
+		var url = "https://api.foursquare.com/v2/venues/search?&query=" + query + "&mode=url&limit=5&near=" + location + "%2C%20CA%2C%20United%20States&client_id=YDAI3HM532ZHCLP4XBC4Z5OCOO2YN5JT3Q3SP4C3EKDNZKT5&client_secret=UQKRY5R4YPYKDQAQNUCVSSVGGPMUXPQA1XSIJOIQT5ZDO4HU&v=20140806&m=foursquare";
 
-//   map = new google.maps.Map(document.getElementById('map'), {
-//       center: home,
-//       zoom: 15
-//     });
+		$.ajax({
 
-//   var request = {
-//     location: home,
-//     radius: '500',
-//     query: 'food'
-//   };
+		url: url,
+		method: "GET"
 
-// service = new google.maps.places.PlacesService(map);
-//   service.textSearch(request, callback);
+		}).done(function (call){
 
+			for (var i = 0; i < 5; i++) {
+				
+				var newDiv = $("<div>");
+				newDiv.addClass("things");
+				newDiv.attr("value", i);
 
-// }
+				p = $("<p>");
+				p2 = $("<p>");
+				p3 = $("<p>");
+				
+				p.append("Name: " + call.response.venues[i].name);
+				p2.append("Location: " + call.response.venues[i].location.formattedAddress);
+				p2.attr("id","locationInfo");
+				p2.attr("location",call.response.venues[i].location.formattedAddress);
+				p3.append("Category: " + call.response.venues[i].categories[0].name);
+				 
+				newDiv.append(p, p2, p3);
 
-// function callback(results, status) {
-//   if (status == google.maps.places.PlacesServiceStatus.OK) {
-//     for (var i = 0; i < results.length; i++) {
-//       var place = results[i];
-//       createMarker(results[i]);
-//     }
-//   }
-// }
+			 	lats = call.response.venues[i].location.lat;
+				lngs = call.response.venues[i].location.lng;
 
+				lat.push(lats);
+				lng.push(lngs);
 
+				$("#list").append(newDiv);
 
+			}
 
+		});
 
+	});
 
+	$(document).on("click", ".things", function() {
+		
+		var pp = $("<p>");
 
+		pp.append("Your Choice: ")
+		$("#thinghere").append(pp);
+		$("#thinghere").append(this);
 
+		$(this).attr("class","selected");
 
+		$("#list").html("");
+		$("#thing1").val("");
 
+		console.log("The Latitude is:")
+		console.log(lat[$(this).attr("value")]);
+		console.log("The Longitude is:")
+		console.log(lng[$(this).attr("value")]);
+	});
 
+	function initMap() {
 
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 6,
+          center: {lat: 41.85, lng: -87.65}
+        });
+        directionsDisplay.setMap(map);
 
+        // document.getElementById('submit').addEventListener('click', function() {
+        //   calculateAndDisplayRoute(directionsService, directionsDisplay);
+        // });
 
+        $("#route").on("click",function (){
+        	calculateAndDisplayRoute(directionsService, directionsDisplay);
+        });
+    }
 
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        
+        var waypts = [];
+        var checkboxArray = $("#locationInfo").attr("location");
+        console.log(checkboxArray);
+        
+        // console.log(checkboxArray);
+        // console.log(checkboxArray.options);
+        
+        // for (var i = 0; i < checkboxArray.length; i++) {
 
+        //   if (checkboxArray.options[i].selected) {
+        //     ///need to push waypoints as objects
+        //     console.log(checkboxArray.options[i].selected);
+        //     console.log(checkboxArray[i].value);
+        //     waypts.push({
+        //       location: checkboxArray[i].value,
+        //       stopover: true
+        //     });
+        //   }
+        // }
 
+        waypts.push({
+              location: checkboxArray,
+              stopover: true
+         });
 
-var start = $("#starts").val();
+        directionsService.route({
 
+          origin: $("#starts").val(),
+          destination: $("#ends").val(),
+          waypoints: waypts,
+          optimizeWaypoints: true,
+          travelMode: 'DRIVING'
 
+        }, 
 
-  $("#sub3").on("click",  function(){
- event.preventDefault();
+        function(response, status) {
 
-// service = new google.maps.places.PlacesService(map);
-// service.nearbySearch(request, callback);
+          if (status === 'OK') {
 
-// Places Search
-// function initialize() {
-//   var home = new google.maps.LatLng(34.052235, -118.243683);
+            directionsDisplay.setDirections(response);
 
-//   map = new google.maps.Map(document.getElementById('map'), {
-//       center: home,
-//       zoom: 15
-//     });
+            var route = response.routes[0];
+            var summaryPanel = document.getElementById('directions-panel');
+            // summaryPanel.innerHTML = '';
 
-//   var request = {
-//     location: home,
-//     radius: '500',
-//     type: ['restaurant']
-//   };
+            // For each route, display summary information.
+            // for (var i = 0; i < route.legs.length; i++) {
+            //   var routeSegment = i + 1;
+            //   summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+            //       '</b><br>';
+            //   summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+            //   summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+            //   summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+            // }
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+          
+        });
+     }
 
-//   service = new google.maps.places.PlacesService(map);
-//   service.nearbySearch(request, callback);
-// }
+     initMap();
 
-// function callback(results, status) {
-//   if (status == google.maps.places.PlacesServiceStatus.OK) {
-//     for (var i = 0; i < results.length; i++) {
-//       var place = results[i];
-//       createMarker(results[i]);
-//     }
-//   }
-// }
-
-
-
-
-
-
-// Maps Search
- var directionsDisplay,
-    directionsService,
-    map;
-
-
-
-var map;
-var service;
-var infowindow;
-
-
-function initialize() {
-  var directionsService = new google.maps.DirectionsService();
-  directionsDisplay = new google.maps.DirectionsRenderer();
-  var home = new google.maps.LatLng(34.052235, -118.243683);
-  var mapOptions = { zoom:7, mapTypeId: google.maps.MapTypeId.ROADMAP, center: home }
-  map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-  directionsDisplay.setMap(map);
-}
-
-      
-
-initialize();
 
 });
-
-
-
-
-// var places = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=food+in+123+main+street&key=AIzaSyDiU0yNUkknW32yOxZpTm4Cii_z8YqttuE";
-
-
-
-//  $.ajax({
-
-
-//   url: places,
-//    method: "GET"
-
-     
-     
-
-
-//  }).done(function(info){
-
-//  var business = ("<p> Name: " + info.results.name);
-//  var rating = ("<p> rating: " + info.results.rating);
-//  var address = ("<p> address: " + info.results.formatted_address);
-
-//  $("#print").append(business, rating, address);
-
-//  });
-
-//  });
-
-
-      
-//  function list(){
-
-// var key = "AIzaSyBm04RwZjjDTqmjQlDjzAH3zSAJwSSlIgs";	
-
-//  var placesUrl = "https://maps.googleapis.com/maps/api/js?key=" + key + "&libraries=places";
-
-
-//  console.log(search);
-
-// }
